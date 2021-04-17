782a371ce2ada76249fc670dbebd92efa450af7a
Rani Saad <rsaad@live.ca>
2021-04-16 21:45:49 -0400

    Validate answers

--- app.js
+++ app.js
@@ -55,14 +55,20 @@ const promptUser = () => {
       },
     },
     {
+      type: "confirm",
+      name: "confirmAbout",
+      message:
+        'Would you like to enter some information about yourself for an "About" section?',
+      default: true,
+    },
+    {
       type: "input",
       name: "about",
-      message: "Provide some information about yourself:(required)",
-      validate: nameInput => {
-        if (nameInput) {
+      message: "Provide some information about yourself:",
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
           return true;
         } else {
-          console.log("Please enter your name!");
           return false;
         }
       },
